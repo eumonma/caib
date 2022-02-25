358604368d941868cea32b82a5033db344b05388
eugenio.montoro <eugenio.montoro@inetum.world>
2022-02-25 15:03:19 +0100

    Añadir Celdas

--- lib/widget/ticket2_widget.dart
+++ lib/widget/ticket2_widget.dart
@@ -26,7 +26,7 @@ class _ticket2WidgetState extends State<ticket2Widget> {
           final tickets = snapshot.data!;
 
           return ListView(
-            children: tickets.map(buildUser).toList(),
+            children: tickets.map(buildUser2).toList(),
           );
         } else {
           return Center(child: CircularProgressIndicator());
@@ -38,6 +38,38 @@ class _ticket2WidgetState extends State<ticket2Widget> {
         title: Text(ticket.descTicket),
         subtitle: Text(ticket.descTicket),
       );
+
+  Widget buildUser2(Ticket ticket) => Row(
+    children: [
+      CircleAvatar(child: Text(ticket.codTicket)),
+      Text(ticket.descTicket),
+      Text(ticket.horasEstimadas.toString()),
+      Text(ticket.horasConsumidas.toString()),
+      celda('Descripción', ticket.descTicket),
+    ],
+    );
+
+  Widget celda(String etiqueta, String dato) => Container(
+    width: 100,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          child: Text(etiqueta,
+            style: TextStyle(
+              fontSize: 10,
+              color: Color.fromARGB(197, 144, 247, 153)),
+          ),
+          alignment: Alignment.topLeft,
+        ),
+        Text(dato,
+          textAlign: TextAlign.start,
+          //style: TextStyle(color: Color.fromARGB(199, 245, 83, 83)),
+        ),
+      ],
+    ),
+  );
 
   Stream<List<Ticket>> readTickets() => FirebaseFirestore.instance
       .collection('Lote1')
